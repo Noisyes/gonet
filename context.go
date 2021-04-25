304444e1328ac5d9072d964cb76726f810d55f2a
孔渝峰 <noisyes@kongyufengdeMacBook-Pro.local>
2021-04-25 10:51:03 +0800

    add context fial method

--- context.go
+++ context.go
@@ -76,6 +76,11 @@ func (c *Context) JSON(code int,obj interface{}){
 	}
 }
 
+func (c *Context) Fail(code int,err string){
+	c.index = len(c.handlers)
+	c.JSON(code,H{"message":err})
+}
+
 func (c *Context) Data(code int,data []byte){
 	c.Status(code)
 	c.Writer.Write(data)
